8bb9bca1ceb78f3359a6eb4960c2dc6eebacb7e4
KS <106144986+strawberryKS@users.noreply.github.com>
2022-12-13 23:35:02 +0900

    Update randam.js

--- randam.js
+++ randam.js
@@ -1,5 +1,5 @@
 function getRandomInt(max){
   return Math.floor(Math.random() * max);
 }
-var links = ['https://forms.gle/WXLg23Aj3dBxHD8i8'];
+var links = ['https://forms.gle/WczztBQ7P6vb3RbQ9','https://forms.gle/EoRhTQA3MWTBtrgu6','https://forms.gle/qCLcCyt4K22GcEye6','https://forms.gle/nGAexCwSPV9TUtWu7','https://forms.gle/hQe81tsFeVWUCt5F9','https://forms.gle/tGmEyGvU9kvYrFZm6','https://forms.gle/DwjUi6dPmMo3R2FdA','https://forms.gle/A5M4MciMVKr3K1xj6','https://forms.gle/WFbdSiUoARGyUqRb8','https://forms.gle/DfCZ14tGKsj6CwAu5','https://forms.gle/aQQTXnYe1NWpYRbeA','https://forms.gle/SNjh5of8uv44KNwr7','https://forms.gle/tsPhkrsxh9R3xiv1A','https://forms.gle/u4uPdTXRMwoEveyYA','',];
 window.location.href = links[getRandomInt(links.length)];
